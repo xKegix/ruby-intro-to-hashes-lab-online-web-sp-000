ab723f3e434bb140930cad7c1be78627c09be31d
Keginold Philip <keginoldp@hotmail.com>
2021-01-29 22:30:58 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -77,13 +77,13 @@ def monopoly_with_fourth_tier
           "mortgage_value" => '$800'
         }
       },
+
       :rent_in_dollars => {
         :one_piece_owned => 25,
         :two_pieces_owned => 50,
         :three_pieces_owned => 100,
         :four_pieces_owned => 200
       }
-  }
   }
 
 
